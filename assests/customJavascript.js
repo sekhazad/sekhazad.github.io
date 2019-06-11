7cef529a714c9727a373d06d0f7991516d31bb7c
Abulkalamazad Sekh <sekhazad740@gmail.com>
2019-06-11 10:11:55 +0530

    assests commit

---
+++ assests/customJavascript.js
@@ -0,0 +1,18 @@
+var granimInstance = new Granim({
+    element: '#canvas-basic',
+    direction: 'diagonal',
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#11998e', '#11998e'],
+                ['#DCE35B', '#45B649'],
+                ['#1D976C', '#93F9B9']
+            ]
+        }
+    }
+});
+
+
+
+
